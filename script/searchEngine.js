6f613e6aab27284df24b4b94d14066c1b80c5a36
Aurélien Arcangeli <arkaurel5929@gmail.com>
2022-08-23 01:07:07 +0200

    Faire le responsive

--- script/searchEngine.js
+++ script/searchEngine.js
@@ -13,7 +13,6 @@ function researchInput(datas) {
     // let datas_filter =[];
     if (value.length <= 2) return datas
     // for (let i = 0; i < datas.length; i++){
-    //     console.log(datas[i])
     //     if (reciperToString(datas[i]).includes(value)) {
            
     //         datas_filter.push(datas[i])
